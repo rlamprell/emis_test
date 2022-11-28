632eeb6c5ceb26f23884884275855090e5234f81
Rob Lamprell <rob.lamprell@hotmail.co.uk>
2022-11-28 16:41:31 +0000

    basic class framework

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+
+
+# import the files
+class Extract:
+    def __init__(self, files):
+        pass
+
+    def get():
+        pass
+
+
+
+# transform the files
+class Transform:
+    def __init_(self, files):
+        pass
+
+
+
+
+# load the files into a db
+class Load:
+    def __init__(self, files):
+        pass
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
