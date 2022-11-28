495370bec6e8ac535c27eed49eb69ce452bf41c1
Rob Lamprell <rob.lamprell@hotmail.co.uk>
2022-11-28 16:57:24 +0000

    looping through each df file

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
+import pandas as pd
 
+import glob
+import os
+
+class File:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_path = file_path
 
 
 
 # import the files
 class Extract:
     def __init__(self, files):
-        pass
+        current_path    = os.getcwd()
+        self.files      = glob.glob(f"{current_path}\data\*.json")
 
-    def get():
-        pass
+    def getFiles(self):
+        return self.files
 
 
 
 # transform the files
 class Transform:
-    def __init_(self, files):
-        pass
+    def __init__(self, files):
+        self.files = files
+
+    def unpack(self):
+
+        for file in self.files:
+            data    = pd.read_json(file)
+            df      = pd.json_normalize(data["entry"])
+
+            print(file)
+            print(type(file))
+            print(data)
+
+
+        return 
 
 
 
@@ -28,7 +50,10 @@ class Load:
 
 
 def main():
-    pass
+    raw_files       = Extract(files=1).getFiles()
+    unpacked_files  = Transform(raw_files).unpack()
+
+
 
 if __name__ == '__main__':
-    pass
+    main()
