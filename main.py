4cff6ae876bf0ea9a606a85c5d3b095683758a3b
Rob Lamprell <rob.lamprell@hotmail.co.uk>
2022-12-03 22:22:42 +0000

    DIP and abstract changees on db_connection.  Exploder method added back in.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from src.transform          import Transform
 # from src.mySQL_connection   import mySQL_connection
 # from src.db_connection      import db_connection
 # from src.db_connections     import db_connection
-from src.db_connections.mySQL_connection import mySQL_connection
+# from src.db_connections.mySQL_connection import mySQL_connection
+from src.db_connections.db_connection import mysql_conn
+from src.db_connections.mysql.db_connection_config import mySQL_connection_details
 
 
 def main():
@@ -15,14 +17,16 @@ def main():
 
     for df in seperated_files:
         current_df = seperated_files[df]
-        # current_df = file_transformer.explode_nested_arrays(current_df, df)
+        current_df = file_transformer.explode_nested_arrays(current_df, df)
         current_df = file_transformer.flatten_df(current_df)
         current_df = current_df.drop_duplicates()
 
-        mySQL_connection().post(df, current_df)
+        connection = mysql_conn(mySQL_connection_details())
+        connection.executePost(table_name=df, df=current_df)
+        # mySQL_connection().post(df, current_df)
         # output = mySQL_connection().get('Patient')
         # print(output)
 
-        
+
 if __name__ == '__main__':
     main()
--- src/db_connections/db_connection.py
+++ src/db_connections/db_connection.py
@@ -5,12 +5,12 @@ import sqlalchemy as db
 from dataclasses import dataclass
 
 from abc import ABC, abstractmethod
-
+from .mysql import db_connection_config
 
 
 class db_connection(ABC):
-    def __init__(self, connection_details):
-        self.config = mySQL_connection_details().config
+    def __init__(self, connection_details: db_connection_config):
+        self.config = connection_details.config
         self._connectionString = self._connectionString()
 
     @abstractmethod
@@ -28,18 +28,18 @@ class db_connection(ABC):
 
 
 
-class db_connection_mysql(db_connection):
+class mysql_conn(db_connection):
     def __init__(self, connection_details):
         super().__init__(connection_details)
 
 
     def _connectionString(self):
-        db_container = self.config.get('container')
-        db_user = self.config.get('user')
-        db_pwd = self.config.get('password')
-        db_host = self.config.get('host')
-        db_port = self.config.get('port')
-        db_name = self.config.get('database')
+        db_container   = self.config.get('container')
+        db_user        = self.config.get('user')
+        db_pwd         = self.config.get('password')
+        db_host        = self.config.get('host')
+        db_port        = self.config.get('port')
+        db_name        = self.config.get('database')
         connection_str = f'{db_container}+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
         return connection_str
 
@@ -50,9 +50,9 @@ class db_connection_mysql(db_connection):
             metadata = db.MetaData(bind=engine)
             metadata.reflect(only=[f'{table}'])
 
-            test_table = metadata.tables[f'{table}']
-            stmt = select(f"{columns}").select_from(test_table)
-            results = connection.execute(stmt).fetchall()
+            test_table  = metadata.tables[f'{table}']
+            stmt        = select(f"{columns}").select_from(test_table)
+            results     = connection.execute(stmt).fetchall()
 
         return results
 
@@ -113,3 +113,47 @@ class mySQL_actions(db_actions):
 
     def delete():
         pass
+
+
+
+class db_connection(db_connection):
+    def __init__(self, connection_details):
+        super().__init__(connection_details)
+
+
+    def _connectionString(self):
+        db_container = self.config.get('container')
+        db_user = self.config.get('user')
+        db_pwd = self.config.get('password')
+        db_host = self.config.get('host')
+        db_port = self.config.get('port')
+        db_name = self.config.get('database')
+        connection_str = f'{db_container}+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
+        return connection_str
+
+
+    def executeGet(self, table, columns="*"):
+        engine = db.create_engine(self._connectionString)
+        with engine.connect() as connection:
+            metadata = db.MetaData(bind=engine)
+            metadata.reflect(only=[f'{table}'])
+
+            test_table = metadata.tables[f'{table}']
+            stmt = select(f"{columns}").select_from(test_table)
+            results = connection.execute(stmt).fetchall()
+
+        return results
+
+
+    def executePost(self, table_name, df):
+        engine = db.create_engine(self._connectionString)
+        with engine.connect() as connection:
+            df.to_sql(table_name, connection, if_exists='replace', index=False)
+
+
+
+
+
+if __name__ == "__main__":
+    connection = mysql_conn(mySQL_connection_details())
+    print(connection.executeGet('Patient'))
