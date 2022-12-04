24bffd9f9751d8599a78e9b853c669a93846f865
Rob Lamprell <rob.lamprell@hotmail.co.uk>
2022-12-04 18:19:23 +0000

    tidying up files

--- py_versions.py
+++
@@ -1,9 +0,0 @@
-import pandas
-print(pandas.__version__)
-import flatten_json
-# print(flatten_json.__version__)
-import sqlalchemy
-print(sqlalchemy.__version__)
-
-import pymysql
-print(pymysql.__version__)
